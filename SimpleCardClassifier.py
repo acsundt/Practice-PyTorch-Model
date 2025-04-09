9e584e6c96b146b08771b2ac49409b7fd03f56fd
aidansundt <aidansundt@gmail.com>
2025-04-09 15:17:11 -0400

    done maybe? Test on PC

---
+++ SimpleCardClassifier.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+
+
+class SimpleCardClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleCardClassifier, self).__init__()
+        # Where we define all the parts of the model
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        enet_out_size = 1280
+        # Make a classifier
+        self.classifier = nn.Linear(enet_out_size, num_classes)
+
+    def forward(self, x):
+        # Connect these parts and return the output
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
--- main.py
+++ main.py
@@ -12,26 +12,94 @@ import numpy as np
 import os
 
 from PlayingCardDataset import PlayingCardDataset
+from SimpleCardClassifier import SimpleCardClassifier
+
+from tqdm.notebook import tqdm
 
 
 def main():
 
-    dataset_path = r"C:\Users\aidan\OneDrive\Documents\ML_Stuff\Card_Images_Dataset\train"
+    model = SimpleCardClassifier(num_classes=53)
 
-    dataset = PlayingCardDataset(data_dir=dataset_path)
-
-    print("length of dataset = ", len(dataset))
-
-    target_to_class = {v: k for k, v in ImageFolder(dataset_path).class_to_idx.items()}
-    # print(target_to_class)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     transform = transforms.Compose([
         transforms.Resize((128, 128)),
         transforms.ToTensor(),
     ])
 
-    dataset = PlayingCardDataset(dataset_path, transform)
-    print(dataset[100])
+    train_path = r"C:\Users\aidan\OneDrive\Documents\ML_Stuff\Card_Images_Dataset\train"
+    valid_path = r"C:\Users\aidan\OneDrive\Documents\ML_Stuff\Card_Images_Dataset\valid"
+    test_path = r"C:\Users\aidan\OneDrive\Documents\ML_Stuff\Card_Images_Dataset\test"
+
+    train_dataset = PlayingCardDataset(data_dir=train_path, transform=transform)
+    val_dataset = PlayingCardDataset(valid_path, transform=transform)
+    test_dataset = PlayingCardDataset(test_path, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    num_epochs = 5
+    train_losses, val_losses = [], []
+
+    model.to(device)
+
+    for epoch in range(num_epochs):
+        # Set model to train
+        model.train()
+        running_loss = 0.0
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * inputs.size(0)
+        train_loss = running_loss / len(train_loader.dataset)
+        train_losses.append(train_loss)
+
+        # Validation phase
+        model.eval()
+        running_loss = 0.0
+        with torch.no_grad():
+            for images, labels in tqdm(val_loader, desc='Validation Loop'):
+                # Move inputs and labels to the device
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item() * inputs.size(0)
+            val_loss = running_loss / len(val_loader.dataset)
+            val_losses.append(val_loss)
+
+        # Print epoch stats
+            print(f"Epoch {epoch+1}/{num_epochs} - Train loss: {train_losses}, Validation loss: {val_losses}")
+
+
+    # print("length of dataset = ", len(dataset))
+
+   #  target_to_class = {v: k for k, v in ImageFolder(dataset_path).class_to_idx.items()}
+    # print(target_to_class)
+
+
+    # dataset = PlayingCardDataset(dataset_path, transform)
+    # print(dataset[100])
+
+    # iterate over dataset
+    # for image, label in dataset:
+      #  break
+
+    # dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    # model = SimpleCardClassifier(num_classes=53)
+    # print("Model: ",model)
+
+
 
 
 
